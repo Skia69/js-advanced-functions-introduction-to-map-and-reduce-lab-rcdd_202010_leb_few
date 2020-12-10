af5ed56f9eeceae317b770a0529c13b03c48a261
Mhamad Ibrahim <skiastep@gmail.com>
2020-12-10 11:38:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -42,9 +42,9 @@ const reduceToTotal = (src, startingPoint = 0) => {
 const reduceToAllTrue = (src) => {
   for (let item of src) { 
     if (!item){ 
-      return true
+      return false
     } 
-    return false
+    return true
   }
 }
 
